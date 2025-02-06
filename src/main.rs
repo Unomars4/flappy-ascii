60972fe297d668e8ce0118788858b171b9852419
Une Nyide <une.nyide@gmail.com>
2025-02-06 16:39:05 +0200

    add modes and enum states to dictate different modes.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,56 @@
 use bracket_lib::prelude::*;
 
 #[derive(Debug)]
-struct State {}
+struct State {
+    mode: GameModes,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            mode: GameModes::Menu,
+        }
+    }
+
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameModes::Playing;
+        ctx.print(1, 1, "Playing");
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        self.mode = GameModes::Menu;
+        ctx.print(1, 1, "Main Menu");
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        self.mode = GameModes::End;
+        ctx.print(1, 1, "Game Over");
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello World 🌍");
+        match self.mode {
+            GameModes::Playing => self.play(ctx),
+            GameModes::Menu => self.main_menu(ctx),
+            GameModes::End => self.dead(ctx),
+        }
     }
 }
 
+#[derive(Debug)]
+enum GameModes {
+    Playing,
+    Menu,
+    End,
+}
+
 fn main() -> BError {
-    println!("Hello, world!");
+    println!("Hello, world! ");
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy ascii 😛")
         .build()?;
 
-    main_loop(context, State {})
+    main_loop(context, State::new())
 }
