43b40c53eeb9a52798f4fa032b0c90563506d45f
Une Nyide <une.nyide@gmail.com>
2025-02-06 16:08:04 +0200

    add bracket-lib dependecy, boilerplate code to start window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use bracket_lib::prelude::*;
+
+#[derive(Debug)]
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello World 🌍");
+    }
+}
+
+fn main() -> BError {
     println!("Hello, world!");
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy ascii 😛")
+        .build()?;
+
+    main_loop(context, State {})
 }
