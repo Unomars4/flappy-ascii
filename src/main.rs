e2186ba56f62d209a93ac394bf88e699c4c095a1
Une Nyide <une.nyide@gmail.com>
2025-02-10 16:05:54 +0200

    add gravity and move method to player struct

--- src/main.rs
+++ src/main.rs
@@ -75,6 +75,17 @@ impl Player {
     fn render(&mut self, ctx: &mut BTerm) {
         ctx.set(self.x, self.y, RED, BLACK, to_cp437('@'));
     }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
 }
 
 fn main() -> BError {
