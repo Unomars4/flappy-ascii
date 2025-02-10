777a6a0c7e021ac02315fdafb67a13e059943e3f
Une Nyide <une.nyide@gmail.com>
2025-02-10 16:00:46 +0200

    add player struct, constructor and render method

--- src/main.rs
+++ src/main.rs
@@ -57,6 +57,26 @@ enum GameModes {
     End,
 }
 
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x: x,
+            y: y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(self.x, self.y, RED, BLACK, to_cp437("@"));
+    }
+}
+
 fn main() -> BError {
     println!("Hello, world! ");
     let context = BTermBuilder::simple80x50()
